7064cbff7c39e79489c54d73f92e049548fe3aca
Venyak <mafia.papyrus@yandex.ru@>
2022-08-28 20:09:04 +0500

    Users

--- src/App.js
+++ src/App.js
@@ -6,10 +6,56 @@ import { Users } from './components/Users';
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+  const [users, setUsers] = React.useState([]);
+  const [invites, setInvites] = React.useState([]);
+  const [isLoading, setLoading] = React.useState(true);
+  const [sent, setSent] = React.useState(false);
+  const [searchValue, setSearchValue] = React.useState('');
+
+  React.useEffect(() => {
+    fetch('https://reqres.in/api/users')
+      .then((res) => res.json())
+      .then((json) => {
+        setUsers(json.data);
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert('Ошибка при получении пользователей');
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  const onChangeSearchValue = (event) => {
+    setSearchValue(event.target.value);
+  };
+
+  const onClickInvites = (id) => {
+    if (invites.includes(id)) {
+      setInvites((prev) => prev.filter((_id) => _id !== id));
+    } else {
+      setInvites((prev) => [...prev, id]);
+    }
+  };
+
+  const onClickSendInvites = () => {
+    setSent(!sent);
+  };
+
   return (
     <div className="App">
-      <Users />
-      {/* <Success /> */}
+      {sent ? (
+        <Success count={invites.length} />
+      ) : (
+        <Users
+          searchValue={searchValue}
+          onChangeSearchValue={onChangeSearchValue}
+          onClickInvites={onClickInvites}
+          onClickSendInvites={onClickSendInvites}
+          invites={invites}
+          items={users}
+          isLoading={isLoading}
+        />
+      )}
     </div>
   );
 }
